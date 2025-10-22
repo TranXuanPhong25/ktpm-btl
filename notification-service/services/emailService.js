2dcc93177fdef0fe8296366fb2178ca0c65a7d8c
Rengumin <tranxuanphong26525@gmail.com>
2025-10-22 14:58:00 +0700

    fix: throw error when send notificaiton

--- notification-service/services/emailService.js
+++ notification-service/services/emailService.js
@@ -20,6 +20,7 @@ const sendEmail = async (to, subject, text) => {
       console.log("Email sent successfully");
    } catch (error) {
       console.error("Error sending email: ", error.message);
+      throw error;
    }
 };
 
